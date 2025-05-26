4925cc1128527f027ea8b443aa7b473696fb32ea
Moskovkin Ilya <m.ilya@brokeronlinexchange.com>
2025-05-27 00:43:43 +0700

    Update db.ts

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,9 +1,12 @@
-import pkg from '@prisma/client'
-const { PrismaClient } = pkg
+import { PrismaClient } from '@prisma/client'
 
-const prisma =
-  process.env.NODE_ENV === 'production'
-    ? new PrismaClient()
-    : (globalThis.__prisma ??= new PrismaClient())
+declare global {
+  var __prisma: PrismaClient | undefined
+}
+
+// reuse global instance in dev
+const prisma = process.env.NODE_ENV === 'production'
+  ? new PrismaClient()
+  : (globalThis.__prisma ??= new PrismaClient())
 
 export default prisma
