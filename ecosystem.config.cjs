b4fa688b175792a72ee036a5a7767bb52d5963dd
Moskovkin Ilya <m.ilya@brokeronlinexchange.com>
2025-05-27 11:00:53 +0700

    fix env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,13 +4,11 @@ module.exports = {
       script : ".output/server/index.mjs", // Path relative to deploy directory
       cwd    : "/var/www/dreamseed", // Set current working directory
       env_file: '.env', // Load environment variables from .env file
-      env: {
-        // Default environment (often not used directly by Nuxt 3/Nitro)
-        // NODE_ENV: "production", // Nitro usually sets this
-      },
+   
             env_production: {
         NODE_ENV: "production",
-        AUTH_TRUST_HOST: "true"
+        AUTH_TRUST_HOST: "true",
+        env_file: '/var/www/dreamseed/.env', // Load environment variables from .env file
         // All other environment variables come from .env file via env_file
       }
     }]
